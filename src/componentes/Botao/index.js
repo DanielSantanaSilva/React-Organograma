491338785f8211667cfda6de521ab2481bfbcbcc
Daniel Santana <daniel.santana.silva@uninove.edu.br>
2022-09-02 16:13:07 -0300

    feature: button
    Road map:
    
    Criei um novo projeto React; Componentes funcionais; Estilizei componentes com CSS; Analise das dependências do projeto; Analise dos comandos disponíveis no package.json. Aprendi como passar props para um componente;
    Renderizamos listas com o .map; Utilizei  elementos aninhados usando a prop children; Ouvimos elementos disparados pelo DOM: o onSubmit do nosso form.

---
+++ src/componentes/Botao/index.js
@@ -0,0 +1,7 @@
+import "./Botao.css";
+
+const Botao = (props) => {
+  return <button className="botao">{props.children}</button>;
+};
+
+export default Botao;
--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,17 +1,14 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  const placeholderModificada = props.placeholder;
 
-    const placeholderModificada = `${props.placeholder}...` 
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input placeholder={placeholderModificada} />
+    </div>
+  );
+};
 
-    return (
-        <div className="campo-texto">
-            <label>
-                {props.label}
-            </label>
-            <input placeholder={placeholderModificada}/>
-        </div>
-    )
-}
-
-export default CampoTexto
+export default CampoTexto;
